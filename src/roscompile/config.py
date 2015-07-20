9311767cd4f878c2796f917a9ac54c543d8e79f3
David Lu <davidvlu@gmail.com>
2015-07-19 23:37:52 -0400

    Initial configuration

---
+++ src/roscompile/config.py
@@ -0,0 +1,19 @@
+import os.path
+import yaml
+
+FILENAME = os.path.expanduser('~/.ros/roscompile.yaml')
+
+class Config(dict):
+    def __init__(self):
+        if os.path.exists(FILENAME):
+            self.update( yaml.load( open( FILENAME ) ) )
+            
+    def should(self, verb):
+        if verb not in self:
+            self[verb] = True
+        return self[verb]
+        
+    def write(self):
+        yaml.dump(dict(self), open(FILENAME, 'w'), default_flow_style=False)  
+
+CFG = Config()
