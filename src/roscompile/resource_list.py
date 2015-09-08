2eecffc828af95b695e59488bfed350b61c3b62b
David Lu <davidvlu@gmail.com>
2015-09-07 20:17:46 -0400

    Cache list of messages

--- src/roscompile/resource_list.py
+++ src/roscompile/resource_list.py
@@ -1,15 +1,23 @@
 import subprocess
 
+def get_output_lines(cmd):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return [s for s in out.split('\n') if len(s)>0]
+    
 PACKAGES = {}
+MESSAGES = set()
 
-p = subprocess.Popen(['rospack', 'list'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = p.communicate()
-
-for line in out.split('\n'):
-    if len(line.strip())==0:
-        continue
+for line in get_output_lines(['rospack', 'list']):
     pkg, folder = line.split()
     PACKAGES[pkg] = folder
     
+for line in get_output_lines(['rosmsg', 'list']):
+    pkg, msg = line.split('/')
+    MESSAGES.add( (pkg, msg) )
+    
 def is_package(pkg):
     return pkg in PACKAGES    
+    
+def is_message(pkg, msg):
+    return (pkg, msg) in MESSAGES
