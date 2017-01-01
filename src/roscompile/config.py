dbf69b95e00e15e58812ed15d206fcbfd6886d4a
David V. Lu <davidvlu@gmail.com>
2016-12-31 20:40:11 -0500

    Reformat config file

--- src/roscompile/config.py
+++ src/roscompile/config.py
@@ -7,11 +7,22 @@ class Config(dict):
     def __init__(self):
         if os.path.exists(FILENAME):
             self.update( yaml.load( open( FILENAME ) ) )
+            if 'flags' not in self:
+                keys = set()
+                flags = {}
+                for key in self:
+                    if key == 'canonical_names':
+                        continue
+                    flags[key] = self[key]
+                    keys.add(key)
+                for key in keys:
+                    del self[key]
+                self['flags'] = flags
 
     def should(self, verb):
-        if verb not in self:
-            self[verb] = True
-        return self[verb]
+        if verb not in self['flags']:
+            self['flags'][verb] = False
+        return self['flags'][verb]
 
     def write(self):
         yaml.dump(dict(self), open(FILENAME, 'w'), default_flow_style=False)
